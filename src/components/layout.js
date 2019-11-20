70e1c9cfa17e47953eb25ea26542ec3bb28c96da
Matthew Crist <hello@matthewcrist.com>
2019-11-20 14:02:21 -0500

    Remove stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,29 +11,6 @@ function Layout({ children }) {
       <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
         {children}
       </main>
-
-      <footer className="bg-blue-700">
-        <nav className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
-          <p className="text-white">
-            Created by{` `}
-            <a
-              className="font-bold no-underline text-white"
-              href="https://bryant.io"
-            >
-              Taylor Bryant
-            </a>
-          </p>
-
-          <p>
-            <a
-              className="font-bold no-underline text-white"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-            >
-              GitHub
-            </a>
-          </p>
-        </nav>
-      </footer>
     </div>
   );
 }

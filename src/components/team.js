279020bab60b5ef67159955ae0817d1e8fef723c
Hondo <hi@hondo.xyz>
2019-12-04 18:42:27 -0500

    Update Josh name

--- src/components/team.js
+++ src/components/team.js
@@ -9,7 +9,7 @@ function Team() {
       <div className={`ct-text-xl md:ct-text-2xl ct-text-primary-dark ct-mb-12`}>The Connecticut Digital Services team is a lean but scrappy team made up of a diverse set of skills and experience.</div>
       <div className={`ct-flex ct-flex-wrap ct--mx-8`}>
         <div className="ct-w-full sm:ct-w-1/2 ct-px-8">
-          <TeamMember name="Josh Gabelle" title="Commissioner, Department of Administrative Services" content="We seek to understand and prioritize the needs of our citizens, businesses and state employees." />
+          <TeamMember name="Josh Geballe" title="Commissioner, Department of Administrative Services" content="We seek to understand and prioritize the needs of our citizens, businesses and state employees." />
         </div>
         <div className="ct-w-full sm:ct-w-1/2 ct-px-8">
           <TeamMember name="Mark Raymond" title="Chief Information Officer" content="We seek to understand and prioritize the needs of our citizens, businesses and state employees." />

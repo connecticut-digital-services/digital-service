a553ccf09247304626f56a3e0ee07e3e402e21cf
Hondo <hi@hondo.xyz>
2019-12-05 10:55:52 -0500

    Update josh name

--- src/components/team.js
+++ src/components/team.js
@@ -9,7 +9,7 @@ function Team() {
       <div className={`ct-text-xl md:ct-text-2xl ct-text-primary-dark ct-w-3/4 ct-mb-12`}>Our lean but scrappy team is made up of a diverse set of skills and experience. </div>
       <div className={`ct-flex ct-flex-wrap ct--mx-8`}>
         <div className="ct-w-full sm:ct-w-1/2 ct-px-8">
-          <TeamMember name="Josh Gabelle" title="Commissioner, Department of Administrative Services" content="We seek to understand and prioritize the needs of our citizens, businesses and state employees." />
+          <TeamMember name="Josh Geballe" title="Commissioner, Department of Administrative Services" content="We seek to understand and prioritize the needs of our citizens, businesses and state employees." />
         </div>
         <div className="ct-w-full sm:ct-w-1/2 ct-px-8">
           <TeamMember name="Mark Raymond" title="Chief Information Officer" content="We seek to understand and prioritize the needs of our citizens, businesses and state employees." />

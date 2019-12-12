ecf00001f1299c2608e4023d9e5e12ab84bfc666
Hondo <hi@hondo.xyz>
2019-12-12 10:57:46 -0500

    Updates works

--- src/templates/work.js
+++ src/templates/work.js
@@ -2,17 +2,25 @@ import React from "react";
 import { graphql } from 'gatsby'
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Block from '../components/block';
 
-function WorkPage({data}) {
+function WorkPage({data, html}) {
   const content = data.markdownRemark;
+
+  console.log(data)
 
   return (
     <Layout>
       <SEO
-        keywords={[`ct digital service`, `connecticut`]}
+        keywords={[`ct digital service`, `connecticut`]} 
         title="Connecticut Digital Service"
       />
-      {html}
+      <Block>
+        <a href="/work">Our Work</a>
+        <h1 className={`ct-text-4xl ct-font-bold`}>{ content.frontmatter.title }</h1>
+        <div className={`ct-mb-10`}>Posted { content.frontmatter.date }</div>
+        <div dangerouslySetInnerHTML={{__html: content.html}}></div>
+      </Block>
     </Layout>
   );
 }
